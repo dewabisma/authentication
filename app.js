6c18af6d7f838c1fcc499d15cc3612b2ff59d32c
Dewa Bisma <idewagedebisma@gmail.com>
2020-11-23 16:47:26 +0800

    add OAuth 2.0 Google Strategy

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
 
 // Bcrypt Options
 // const saltRounds = 10;
@@ -45,10 +48,12 @@ mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // userSchema.plugin(encrypt, {
 //     secret: secret,
@@ -59,13 +64,47 @@ const User = new mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: 'http://localhost:5050/auth/google/secrets'
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({
+            googleId: profile.id
+        }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 // Set Up Route
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile']
+}));
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', {
+        failureRedirect: '/login'
+    }),
+    (req, res) => {
+        // Successful authentication, redirect to secrets.
+        res.redirect('/secrets');
+    });
 
 app.get('/login', (req, res) => {
     res.render('login');
@@ -74,7 +113,7 @@ app.get('/login', (req, res) => {
 app.post('/login', (req, res) => {
     const userData = req.body;
 
-    const user = new User ({
+    const user = new User({
         username: userData.username,
         password: userData.password
     });
@@ -83,11 +122,11 @@ app.post('/login', (req, res) => {
         if (err) {
             res.redirect('login');
         } else {
-            passport.authenticate('local')(req, res, ()=> {
+            passport.authenticate('local')(req, res, () => {
                 res.redirect('secrets');
             });
         }
-    }); 
+    });
 });
 
 app.get('/register', (req, res) => {
@@ -110,7 +149,7 @@ app.post('/register', (req, res) => {
 });
 
 app.get('/secrets', (req, res) => {
-    if(req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
         res.render('secrets');
     } else {
         res.redirect('login');
