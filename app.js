ab7696d11a4f57405b2b0c31dbf1bde2c4c9589b
Dewa Bisma <idewagedebisma@gmail.com>
2020-11-23 18:08:52 +0800

    add facebok OAuth, remember to fix mongoose duplicate null error

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
 
@@ -49,7 +50,8 @@ mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    facebookId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -88,6 +90,21 @@ passport.use(new GoogleStrategy({
     }
 ));
 
+passport.use(new FacebookStrategy({
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: "http://localhost:5050/auth/facebook/secrets"
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        User.findOrCreate({
+            facebookId: profile.id
+        }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
 // Set Up Route
 app.get('/', (req, res) => {
     res.render('home');
@@ -99,6 +116,17 @@ app.get('/auth/google', passport.authenticate('google', {
 
 app.get('/auth/google/secrets',
     passport.authenticate('google', {
+        failureRedirect: '/login'
+    }),
+    (req, res) => {
+        // Successful authentication, redirect to secrets.
+        res.redirect('/secrets');
+    });
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+    passport.authenticate('facebook', {
         failureRedirect: '/login'
     }),
     (req, res) => {
